849576ac499f92aa9625ff79cd73a0b314c69401
Colby <colbytaylor1212@gmail.com>
2019-03-15 13:21:10 -0400

    restyled index and blog as well as added new css file

--- js/Main.js
+++ js/Main.js
@@ -1,1 +1,9 @@
-var person = {};
+function show(shown, hidden) {
+    document.getElementById(shown).style.display='block';
+    document.getElementById(hidden).style.display='none';
+    return false;
+}
+
+/*function showAll() {
+    document.getElementById().style.disply='block';
+}*/
